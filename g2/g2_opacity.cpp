5ad15e612bc03c44c75c1044ae2804de4bb4c505
afshin.mortazavi-nia@intel.com <afshin.mortazavi-nia@intel.com>
2016-04-04 12:59:03 -0700

    removed opacity stack

--- g2/g2_opacity.cpp
+++ g2/g2_opacity.cpp
@@ -4,40 +4,18 @@
 
 namespace g2 {
 	namespace Internal {
-		float alpha1_parent = 1.f;
 		float alpha1_effective = 1.f;
 		float alpha2_effective = 1.f;
-
-		std::stack<float> alphaStack;
 	}
 }
 
 using namespace g2;
 using namespace g2::Internal;
 
-void g2::pushOpacity() {
-	alphaStack.push(alpha2_effective);
-	alphaStack.push(alpha1_effective);
-	alphaStack.push(alpha1_parent);
-
-	alpha1_parent = alpha1_effective;
-}
-
-void g2::popOpacity() {
-	alpha1_parent = alphaStack.top();
-	alphaStack.pop();
-
-	alpha1_effective = alphaStack.top();
-	alphaStack.pop();
-
-	alpha2_effective = alphaStack.top();
-	alphaStack.pop();
-}
-
 void g2::opacity(float alpha) {
-	g2::Internal::alpha1_effective = g2::Internal::alpha1_parent * alpha;
+	alpha1_effective = alpha;
 }
 
 void g2::opacity2(float alpha) {
-	alpha2_effective = alpha1_parent * alpha;
+	alpha2_effective = alpha;
 }
--- g2/g2_opacity.h
+++ g2/g2_opacity.h
@@ -2,10 +2,7 @@
 
 namespace g2 {
 	namespace Internal {
-		extern float alpha1_parent;
 		extern float alpha1_effective;
 		extern float alpha2_effective;
-
-		extern std::stack<float> alphaStack;
 	}
 }
