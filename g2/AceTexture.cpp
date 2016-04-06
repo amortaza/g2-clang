c7bb75f7ea64821faad1f593cce2ec88933ee390
amortaza <davidnia1@gmail.com>
2016-04-05 18:30:02 -0700

    moved soil out of AceTexture.hpp

---
+++ g2/AceTexture.cpp
@@ -0,0 +1,72 @@
+#include "stdafx.h"
+
+#include "g2/AceTexture.hpp"
+
+AceTexture::AceTexture() {
+	glGenTextures(1, &tid);
+
+	glBindTexture(GL_TEXTURE_2D, tid);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
+AceTexture::AceTexture(int w, int h) {
+	this->w = w;
+	this->h = h;
+
+	glGenTextures(1, &tid);
+
+	glBindTexture(GL_TEXTURE_2D, tid);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
+AceTexture::~AceTexture() {
+	if (imageData) {
+		delete imageData;
+	}
+
+	glDeleteTextures(1, &tid);
+}
+
+void AceTexture::loadRgb(const char* filename) {
+	imageData = SOIL_load_image(filename, &w, &h, 0, SOIL_LOAD_RGB);
+
+	glBindTexture(GL_TEXTURE_2D, tid);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+}
+
+void AceTexture::loadRgba(const char* filename) {
+	imageData = SOIL_load_image(filename, &w, &h, 0, SOIL_LOAD_AUTO);
+
+	glBindTexture(GL_TEXTURE_2D, tid);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+}
+
+void AceTexture::loadAlpha(unsigned char* buffer, int w, int h) {
+
+	glBindTexture(GL_TEXTURE_2D, tid);
+
+	this->w = w;
+	this->h = h;
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, w, h, 0, GL_ALPHA, GL_UNSIGNED_BYTE, buffer);
+}
+
+void AceTexture::activate() {
+	glBindTexture(GL_TEXTURE_2D, tid);
+}
+
+void AceTexture::deactivate() {
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
--- g2/g2/AceTexture.hpp
+++ g2/g2/AceTexture.hpp
@@ -7,74 +7,20 @@ public:
 	GLuint tid;
 
 public:
-    AceTexture() {
-        glGenTextures(1, &tid);
+	AceTexture();
+	AceTexture(int w, int h);
 
-        glBindTexture(GL_TEXTURE_2D, tid);
+	~AceTexture();
 
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    }
+	void loadRgb(const char* filename);
 
-    AceTexture(int w, int h) {
-        this->w = w;
-        this->h = h;
+	void loadRgba(const char* filename);
 
-        glGenTextures(1, &tid);
-
-        glBindTexture(GL_TEXTURE_2D, tid);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    }
-
-    ~AceTexture() {
-        if (imageData) {
-            delete imageData;
-        }
-
-        glDeleteTextures(1, &tid);
-    }
-
-    void loadRgb(const char* filename) {
-        imageData = SOIL_load_image(filename, &w, &h, 0, SOIL_LOAD_RGB);
-
-        glBindTexture(GL_TEXTURE_2D, tid);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
-    }
-
-	void loadRgba(const char* filename) {
-		imageData = SOIL_load_image(filename, &w, &h, 0, SOIL_LOAD_AUTO);
-
-		glBindTexture(GL_TEXTURE_2D, tid);
-
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
-	}
-
-	void loadAlpha(unsigned char* buffer, int w, int h) {
-
-		glBindTexture(GL_TEXTURE_2D, tid);
-
-		this->w = w;
-		this->h = h;
-
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, w, h, 0, GL_ALPHA, GL_UNSIGNED_BYTE, buffer);
-	}
+	void loadAlpha(unsigned char* buffer, int w, int h);
 	
-	void activate() {
-        glBindTexture(GL_TEXTURE_2D, tid);
-    }
+	void activate();
 
-	void deactivate() {
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
+	void deactivate();
 
 private:
 	unsigned char* imageData = 0;
