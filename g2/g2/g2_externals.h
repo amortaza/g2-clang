3a722c3022fb2696eb2efa8788096086a94d0840
amortaza <davidnia1@gmail.com>
2016-04-06 22:03:01 -0700

    added G2_COLOR_ANY, G2_ALPHA_ANY

--- g2/g2/g2_externals.h
+++ g2/g2/g2_externals.h
@@ -21,28 +21,29 @@ namespace g2 {
 		extern int G2_COLOR_SOLID;
 		extern int G2_COLOR_HORIZ_GRADIENT;
 		extern int G2_COLOR_VERT_GRADIENT;
+		extern int G2_COLOR_ANY;
 
 		extern int G2_ALPHA_NONE;
 		extern int G2_ALPHA_SOLID;
 		extern int G2_ALPHA_HORIZ_GRADIENT;
 		extern int G2_ALPHA_VERT_GRADIENT;
+		extern int G2_ALPHA_ANY;
 
-		extern int G2_PAD_ALL; 
+		extern int G2_PAD_ALL;
 		extern int G2_PAD_LEFT;
 		extern int G2_PAD_TOP;
 		extern int G2_PAD_RIGHT;
 		extern int G2_PAD_BOTTOM;
+		extern int G2_PAD_ANY;
 
 		extern int G2_BORDER_ALL;
 		extern int G2_BORDER_LEFT;
 		extern int G2_BORDER_TOP;
 		extern int G2_BORDER_RIGHT;
 		extern int G2_BORDER_BOTTOM;
+		extern int G2_BORDER_ANY;
 
 		extern int G2_MASK;
-
-		extern int G2_COLOR_ANY;
-		extern int G2_PAD_ANY;
 	}
 
 	namespace Internal {
