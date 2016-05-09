dd6a639f58e81f576533bb01bb50e6f719442838
amortaza <davidnia1@gmail.com>
2016-05-08 18:09:07 -0700

    fixed memory leak

--- g2/g2/g2.h
+++ g2/g2/g2.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "TextureRef.h" // memory leak happens otherwise!
+#include "CanvasRef.h" // memory leak happens otherwise!
 
 #include "g2_texture.h"
 #include "g2_canvas.h"
